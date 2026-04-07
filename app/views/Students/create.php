7130ef951bc8748e92afd628e18bc8eb7efc7502
PWarwq <vilux456@gmail.com>
2026-04-07 13:06:56 +0700

    feat: fix navbar

--- app/views/Students/create.php
+++ app/views/Students/create.php
@@ -10,7 +10,7 @@
     <header class="bg-blue-500 text-white">
         <div class="flex justify-between container mx-auto p-4 py-6">
             <a href="/students" class="font-bold text-2xl">Sistem Sekolah</a>
-            <a href="/students/create" class="text-xl bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
+            <a href="/students/create" class="text- xl bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
         </div>
     </header>
 

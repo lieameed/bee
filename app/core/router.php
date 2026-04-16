2f3d3c7f9fd9d4b67e57258bd06f0d3009925e96
PWarwq <vilux456@gmail.com>
2026-04-16 13:46:57 +0700

    feat: setup method spoofing

--- app/core/router.php
+++ app/core/router.php
@@ -9,7 +9,8 @@ class Router
 {
 
     private array $routes = [];
-    public function add (string $method, string $uri, string $controller, string $function){
+    public function add(string $method, string $uri, string $controller, string $function)
+    {
         $this->routes[] = [
             'method' => $method,
             'uri' => $uri,
@@ -20,36 +21,41 @@ class Router
     public function run()
     {
         $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method === 'POST' && isset($_POST['_method'])) {
+            $this->$_POST['method'];
+        }
+
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-            foreach($this->routes as $route){
-                $pattern = str_replace(
-                    '{id}',
-                    '([0-9]+)',
-                    $route['uri']
-                );
+        foreach ($this->routes as $route) {
+            $pattern = str_replace(
+                '{id}',
+                '([0-9]+)',
+                $route['uri']
+            );
 
-                $pattern = '#^' . $pattern . '$#';
+            $pattern = '#^' . $pattern . '$#';
 
-                if(preg_match($pattern, $uri, $matches)){
-                    require_once '../app/controllers/'. $route ['controller'] .'.php';
-                    array_shift($matches);
-                    $controllerClass = 'app\\controllers\\' . $route['controller'];
+            if ($method === $route['method'] && preg_match($pattern, $uri, $matches)) {
+                require_once '../app/controllers/' . $route['controller'] . '.php';
+                array_shift($matches);
+                $controllerClass = 'app\\controllers\\' . $route['controller'];
 
-                    $controller = new $controllerClass();
+                $controller = new $controllerClass();
 
-                    $function = $route['function'];
-                    call_user_func_array([$controller, $function], $matches);
-                    return;
-                }
-
-                // $controllerClass = 'app\\controllers\\' . $route['controller'];
-
-                // $controller = new $controllerClass();
-
-                // $function = $route['function'];
-                // $controller -> $function();
+                $function = $route['function'];
+                call_user_func_array([$controller, $function], $matches);
+                return;
             }
+
+            // $controllerClass = 'app\\controllers\\' . $route['controller'];
+
+            // $controller = new $controllerClass();
+
+            // $function = $route['function'];
+            // $controller -> $function();
+        }
 
         // echo "{$method} {$uri}";
         // hhtps://google.com/search
@@ -72,7 +78,7 @@ class Router
         http_response_code(404);
         echo '<h1>404 - Page Not Found';
 
-    
+
     }
 }
 
